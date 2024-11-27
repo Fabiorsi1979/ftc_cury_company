a4a3cb5b523cd1f7d6a0f19e54869e53619c5c05
Fabio Silva <124393981+Fabiorsi1979@users.noreply.github.com>
2024-11-27 20:46:24 -0300

    Update Home.py
    Corrigindo caminho para imagem

--- Home.py
+++ Home.py
@@ -7,8 +7,7 @@ st.set_page_config(
     page_title='Home',
 )
 
-image_path = 'img/ds.png'
-image = Image.open( image_path)
+image = Image.open( "../img/ds.png" )
 st.logo( image)
 
 st.sidebar.markdown( """___""" )
